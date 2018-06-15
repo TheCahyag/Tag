98457d9416ea1c60d41832e1160b66758932128a
TheCahyag <brandonnavel@outlook.com>
2018-06-14 20:48:42 -0400

    Created Identifiable interface for things that have names and UUIDs

---
+++ tag-common/src/main/java/com/servegame/bl4de/common/model/Identifiable.java
@@ -0,0 +1,14 @@
+package com.servegame.bl4de.common.model;
+
+import java.util.UUID;
+
+/**
+ * File: Identifiable.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public interface Identifiable {
+    String getName();
+
+    UUID getUUID();
+}
