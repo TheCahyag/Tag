f9cdf9f4958fa3f6ea8e55c87f93dd87124ad0fa
TheCahyag <brandonnavel@outlook.com>
2018-06-22 22:42:40 -0400

    Added Sponge's world to SpongeWorld as state and implemented relevant methods

--- tag-sponge/src/main/java/com/servegame/bl4de/tag/model/SpongeWorld.java
+++ tag-sponge/src/main/java/com/servegame/bl4de/tag/model/SpongeWorld.java
@@ -3,7 +3,23 @@ package com.servegame.bl4de.tag.model;
 
 import com.servegame.bl4de.common.model.World;
 
+import java.util.UUID;
+
 public class SpongeWorld implements World {
 
+    private org.spongepowered.api.world.World world;
 
+    public SpongeWorld(org.spongepowered.api.world.World world){
+        this.world = world;
+    }
+
+    @Override
+    public String getName() {
+        return this.world.getName();
+    }
+
+    @Override
+    public UUID getUUID() {
+        return this.world.getUniqueId();
+    }
 }
