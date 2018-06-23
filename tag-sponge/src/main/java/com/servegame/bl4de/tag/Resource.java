2c93410c89a2132c50eb7e4255f1a65efe8b8c8c
TheCahyag <brandonnavel@outlook.com>
2018-06-22 22:39:54 -0400

    Added Resource.java file in tag-sponge for plugin parameters (like name, author, etc)

---
+++ tag-sponge/src/main/java/com/servegame/bl4de/tag/Resource.java
@@ -0,0 +1,17 @@
+package com.servegame.bl4de.tag;
+
+/**
+ * File: Resource.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public class Resource {
+
+    public final static String NAME = "@name@";
+    public final static String ID = "tag";
+    public final static String VERSION = "@version@";
+    public final static String AUTHORS = "TheCahyag";
+    public final static String URL = "@url@";
+    public final static String DESCRIPTION = "@description@";
+
+}
