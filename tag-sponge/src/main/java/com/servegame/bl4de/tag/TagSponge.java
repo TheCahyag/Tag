8ea4d67779430f6b039dd91891e8dfb174901f27
TheCahyag <brandonnavel@outlook.com>
2018-06-22 22:44:11 -0400

    Added a SpongeGameManager as a state for the TagSponge plugin, initialized other state

--- tag-sponge/src/main/java/com/servegame/bl4de/tag/TagSponge.java
+++ tag-sponge/src/main/java/com/servegame/bl4de/tag/TagSponge.java
@@ -1,22 +1,32 @@
 package com.servegame.bl4de.tag;
 
 import com.google.inject.Inject;
+import com.servegame.bl4de.common.AbstractGameManager;
+import com.servegame.bl4de.tag.command.StartGame;
+import com.servegame.bl4de.tag.minigame.SpongeGameManager;
 import org.slf4j.Logger;
 import org.spongepowered.api.Game;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.spec.CommandSpec;
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameConstructionEvent;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.plugin.PluginContainer;
-import org.spongepowered.api.text.Text;
 
+
+/**
+ * File: TagSponge.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
 @Plugin(
-        id = "tag",
-        name = "Tag",
-        description = "A Simple Tag Game for Minecraft",
-        url = "https://github.com/TheCahyag/Tag",
-        authors = {
-                "TheCahyag"
-        }
+        id = Resource.ID,
+        name = Resource.NAME,
+        description = Resource.DESCRIPTION,
+        version = Resource.VERSION,
+        url = Resource.URL,
+        authors = Resource.AUTHORS
 )
 public class TagSponge {
 
@@ -24,11 +34,32 @@ public class TagSponge {
     public static TagSponge instance;
     public static PluginContainer plugin;
 
+    private AbstractGameManager gameManager;
+
     @Inject
     private Game game;
 
     @Listener
+    public void onConstruction(GameConstructionEvent event){
+        instance = this;
+        plugin = Sponge.getPluginManager().getPlugin(Resource.ID).get();
+        gameManager = new SpongeGameManager(instance);
+    }
+
+    @Listener
     public void onServerStart(GameStartedServerEvent event) {
-        game.getServer().getConsole().sendMessage(Text.of("Hello world from plugin"));
+
+        CommandSpec startGame = CommandSpec.builder()
+                .executor(new StartGame())
+                .build();
+        Sponge.getCommandManager().register(plugin, startGame, "startgame");
+
+
+
+    }
+
+
+    public AbstractGameManager getGameManager() {
+        return gameManager;
     }
 }
