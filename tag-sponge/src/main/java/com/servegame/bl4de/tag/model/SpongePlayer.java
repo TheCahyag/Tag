a7e730d1abe4e78d3f2f8e1eef9fc9c4e062120e
TheCahyag <brandonnavel@outlook.com>
2018-06-22 22:42:06 -0400

    Implemented needed methods for the SpongePlayer

--- tag-sponge/src/main/java/com/servegame/bl4de/tag/model/SpongePlayer.java
+++ tag-sponge/src/main/java/com/servegame/bl4de/tag/model/SpongePlayer.java
@@ -1,7 +1,34 @@
 package com.servegame.bl4de.tag.model;
 
 
-import com.servegame.bl4de.common.model.Player;
+import com.flowpowered.math.vector.Vector3d;
+import com.servegame.bl4de.common.model.AbstractPlayer;
+import com.servegame.bl4de.common.model.location.Vector3D;
+import org.spongepowered.api.entity.living.player.Player;
 
-public class SpongePlayer implements Player {
+import java.util.UUID;
+
+public class SpongePlayer extends AbstractPlayer {
+
+    private final Player player;
+
+    public SpongePlayer(Player player){
+        this.player = player;
+    }
+
+    @Override
+    public Vector3D getPosition() {
+        Vector3d vector = this.player.getLocation().getPosition();
+        return new Vector3D(vector.getX(), vector.getY(), vector.getZ());
+    }
+
+    @Override
+    public String getName() {
+        return this.player.getName();
+    }
+
+    @Override
+    public UUID getUUID() {
+        return this.player.getUniqueId();
+    }
 }
