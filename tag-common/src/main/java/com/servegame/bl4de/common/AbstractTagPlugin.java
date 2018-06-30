2eaa693fdc616c95c69a1bc9d870236ba7a3f441
TheCahyag <brandonnavel@outlook.com>
2018-06-30 18:16:33 -0400

    Created AbstractTagPlugin which implements TagPlugin

---
+++ tag-common/src/main/java/com/servegame/bl4de/common/AbstractTagPlugin.java
@@ -0,0 +1,19 @@
+package com.servegame.bl4de.common;
+
+import com.sun.istack.internal.NotNull;
+
+/**
+ * File: TagPlugin.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public class AbstractTagPlugin implements TagPlugin {
+
+    @NotNull
+    protected static AbstractGameManager gameManager;
+
+    @NotNull
+    public static AbstractTagPlugin instance;
+
+    // protected AbstractCommandManager commandManager;
+}
