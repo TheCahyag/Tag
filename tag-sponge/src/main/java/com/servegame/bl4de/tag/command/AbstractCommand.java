043fd49fff68d6f74ab147baa657e8af26b97dae
TheCahyag <brandonnavel@outlook.com>
2018-06-30 18:14:14 -0400

    Created abstract method getCommandUsage and added the registration of the command with the command manager in the constructor

--- tag-sponge/src/main/java/com/servegame/bl4de/tag/command/AbstractCommand.java
+++ tag-sponge/src/main/java/com/servegame/bl4de/tag/command/AbstractCommand.java
@@ -1,9 +1,12 @@
 package com.servegame.bl4de.tag.command;
 
+import com.servegame.bl4de.common.TagPlugin;
+import com.servegame.bl4de.tag.TagSponge;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.text.Text;
 
 /**
  * File: AbstractCommand.java
@@ -15,17 +18,16 @@ public abstract class AbstractCommand<T extends CommandSource> implements Comman
     protected T src;
     protected CommandContext args;
 
-    public AbstractCommand(T src, CommandContext args) {
+    public AbstractCommand(T src, CommandContext args, TagPlugin plugin) {
         this.src = src;
         this.args = args;
+        TagSponge.tagSponge.getTagCommandManager().registerCommand(plugin, this);
     }
 
     /**
-     * Checks to see if the {@link CommandSource} has permission to run the current command
-     * @return true if the src has permission false otherwise
+     * TODO
+     * @return TODO
      */
-    public abstract boolean checkPermission();
-
     public CommandResult runCommand(){
         if (!this.checkPermission()){
             System.out.println("user doesn't have permission");
@@ -34,6 +36,24 @@ public abstract class AbstractCommand<T extends CommandSource> implements Comman
         return execute(this.src, this.args);
     }
 
+    /**
+     * Checks to see if the {@link CommandSource} has permission to run the current command
+     * @return true if the src has permission false otherwise
+     */
+    public abstract boolean checkPermission();
+
     @Override
     public abstract CommandResult execute(CommandSource src, CommandContext args);
+
+    /**
+     * TODO
+     * @return TODO
+     */
+    public abstract Text getCommandUsage();
+
+    // Temp
+
+    public void hello(){
+        this.src.sendMessage(Text.of("Hello from: " + this.getClass()));
+    }
 }
