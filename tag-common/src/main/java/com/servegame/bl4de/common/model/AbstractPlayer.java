fecdfb03cec1b241ecd86833e228004ad6d90bcb
TheCahyag <brandonnavel@outlook.com>
2018-06-29 20:45:35 -0400

    Added observer state to the player (if the player is an observer of a game or is an active player)

--- tag-common/src/main/java/com/servegame/bl4de/common/model/AbstractPlayer.java
+++ tag-common/src/main/java/com/servegame/bl4de/common/model/AbstractPlayer.java
@@ -5,6 +5,16 @@ import com.servegame.bl4de.common.model.location.Vector3D;
 
 public abstract class AbstractPlayer implements Identifiable {
 
+    /**  TODO  **/
+    protected boolean isObserver;
+
+    public AbstractPlayer() {
+        this.isObserver = false;
+    }
+
     public abstract Vector3D getPosition();
 
+    public boolean isObserver() {
+        return isObserver;
+    }
 }
