d5919f9c500852764cb8fd67aacbf49b12686d98
TheCahyag <brandonnavel@outlook.com>
2018-06-29 20:48:30 -0400

    Added Permissions and Descriptions class to contain static strings that represent a commands permission value or description value

---
+++ tag-common/src/main/java/com/servegame/bl4de/common/command/Descriptions.java
@@ -0,0 +1,22 @@
+package com.servegame.bl4de.common.command;
+
+/**
+ * File: Descriptions.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public final class Descriptions {
+
+
+
+    public static final String ADD_PLAYER_TO_GAME = "addplayer";
+    public static final String JOIN_GAME = "join";
+    public static final String LIST_GAMES = "list";
+    public static final String LIST_GAME_INFO = "info";
+
+
+    public static final String CREATE_GAME = "create";
+    public static final String START_GAME = "start";
+    public static final String END_GAME = "stop";
+
+}
---
+++ tag-common/src/main/java/com/servegame/bl4de/common/command/Permissions.java
@@ -0,0 +1,28 @@
+package com.servegame.bl4de.common.command;
+
+/**
+ * File: Permissions.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public final class Permissions {
+
+    // All permissions
+    private static final String TAG_PRE = "tag.";
+
+    // Base permissions
+    private static final String BASE_PRE = TAG_PRE + "base.";
+
+    public static final String ADD_PLAYER_TO_GAME = BASE_PRE + "addplayer";
+    public static final String JOIN_GAME = BASE_PRE + "join";
+    public static final String LIST_GAMES = BASE_PRE + "list";
+    public static final String LIST_GAME_INFO = BASE_PRE + "info";
+
+    // Action permissions
+    private static final String ACTION_PRE = TAG_PRE + "action.";
+
+    public static final String CREATE_GAME = ACTION_PRE + "create";
+    public static final String START_GAME = ACTION_PRE + "start";
+    public static final String END_GAME = ACTION_PRE + "stop";
+
+}
