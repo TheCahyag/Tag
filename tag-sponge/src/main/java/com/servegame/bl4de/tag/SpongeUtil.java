91922f6d59300528f62ddbebb17f8ccd3ef966f2
TheCahyag <brandonnavel@outlook.com>
2018-06-30 18:22:00 -0400

    Added help CommandSpec and updated executors for all commands

--- tag-sponge/src/main/java/com/servegame/bl4de/tag/SpongeUtil.java
+++ tag-sponge/src/main/java/com/servegame/bl4de/tag/SpongeUtil.java
@@ -3,10 +3,9 @@ package com.servegame.bl4de.tag;
 import com.servegame.bl4de.common.TagPlugin;
 import com.servegame.bl4de.common.command.Descriptions;
 import com.servegame.bl4de.common.command.Permissions;
+import com.servegame.bl4de.tag.command.CommandGateKeeper;
 import com.servegame.bl4de.tag.command.element.SpongeDuration;
-import com.servegame.bl4de.tag.command.minigame.CreateGame;
-import com.servegame.bl4de.tag.command.minigame.EndGame;
-import com.servegame.bl4de.tag.command.minigame.StartGame;
+import com.servegame.bl4de.tag.command.minigame.*;
 import org.spongepowered.api.command.CommandManager;
 import org.spongepowered.api.command.spec.CommandSpec;
 import org.spongepowered.api.text.Text;
@@ -30,7 +29,6 @@ public class SpongeUtil {
         // /tag create -m# -t#<smh>
         CommandSpec create = CommandSpec.builder()
                 .arguments(
-                        literal(Text.of("create")),
                         string(Text.of("game_name")),
                         firstParsing(
                                 flags()
@@ -43,40 +41,42 @@ public class SpongeUtil {
                 )
                 .description(Text.of(Descriptions.CREATE_GAME))
                 .permission(Permissions.CREATE_GAME)
-                .executor((src, args) -> new CreateGame(src, args).runCommand())
+                .executor((src, args) -> new CreateGame(src, args, plugin).runCommand())
                 .build();
 
         // /tag start <game_name>
         CommandSpec start = CommandSpec.builder()
                 .arguments(
-                        literal(Text.of("start")),
                         string(Text.of("game_name"))
                 )
                 .description(Text.of(Descriptions.START_GAME))
                 .permission(Permissions.START_GAME)
-                .executor((src, args) -> new StartGame(src, args).runCommand())
+                .executor((src, args) -> new StartGame(src, args, plugin).runCommand())
                 .build();
 
         // /tag end <game_name> [-f]
         CommandSpec end = CommandSpec.builder()
                 .arguments(
-                        literal(Text.of("end")),
                         string(Text.of("game_name")),
                         optional(flags().flag("f").buildWith(none()))
                 )
                 .description(Text.of(Descriptions.END_GAME))
                 .permission(Permissions.END_GAME)
-                .executor((src, args) -> new EndGame(src, args).runCommand())
+                .executor((src, args) -> new EndGame(src, args, plugin).runCommand())
                 .build();
 
         // /tag list
         CommandSpec listGames = CommandSpec.builder()
-                .arguments(
-                        literal(Text.of("list"))
-                )
                 .description(Text.of(Descriptions.LIST_GAMES))
                 .permission(Permissions.LIST_GAMES)
-                .executor((src, args) -> new EndGame(src, args).runCommand())
+                .executor((src, args) -> new ListGames(src, args, plugin).runCommand())
+                .build();
+
+        // /tag help
+        CommandSpec help = CommandSpec.builder()
+                .description(Text.of(Descriptions.HELP))
+                .permission(Permissions.HELP)
+                .executor((src, args) -> new Help(src, args, plugin).runCommand())
                 .build();
 
         // /tag ...
@@ -85,26 +85,26 @@ public class SpongeUtil {
                 .child(start, "start")
                 .child(end, "end")
                 .child(listGames, "list")
+                .child(help, "help")
                 .arguments(
+                        // /tag <game_name> ...
+                        string(Text.of("game_name")),
                         optional(
-                                // /tag <game_name> ...
-                                string(Text.of("game_name")),
-                                optional(
-                                        firstParsing(
-                                                // /tag <game_name> add <player_name>
-                                                seq(
-                                                        literal(Text.of("add"), "add"),
-                                                        player(Text.of("player_name"))
-                                                ),
-                                                // /tag <game_name> join [-f]
-                                                seq(
-                                                        literal(Text.of("join"), "join"),
-                                                        optional(flags().flag("f").buildWith(none()))
-                                                )
+                                firstParsing(
+                                        // /tag <game_name> add <player_name>
+                                        seq(
+                                                literal(Text.of("add"), "add"),
+                                                player(Text.of("player_name"))
+                                        ),
+                                        // /tag <game_name> join [-f]
+                                        seq(
+                                                literal(Text.of("join"), "join"),
+                                                optional(flags().flag("f").buildWith(none()))
                                         )
                                 )
                         )
                 )
+                .executor((src, args) -> new CommandGateKeeper(src, args, plugin).runCommand())
                 .build();
         commandManager.register(plugin, tag, "tag");
     }
