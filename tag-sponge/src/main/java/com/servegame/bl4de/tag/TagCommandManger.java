9406d1536bad9d348c6108cc4e4fe3e7fb5a506c
TheCahyag <brandonnavel@outlook.com>
2018-06-30 18:22:11 -0400

    Created TagCommandManager

---
+++ tag-sponge/src/main/java/com/servegame/bl4de/tag/TagCommandManger.java
@@ -0,0 +1,9 @@
+package com.servegame.bl4de.tag;
+
+/**
+ * File: TagCommandManger.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public class TagCommandManger extends AbstractCommandManager {
+}
