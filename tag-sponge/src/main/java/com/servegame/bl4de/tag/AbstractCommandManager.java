bb365b838c410cdcdedb7d5df6779bb3747474e8
TheCahyag <brandonnavel@outlook.com>
2018-07-22 22:14:08 -0400

    Changed the Set to hold commands instead of Text

--- tag-sponge/src/main/java/com/servegame/bl4de/tag/AbstractCommandManager.java
+++ tag-sponge/src/main/java/com/servegame/bl4de/tag/AbstractCommandManager.java
@@ -1,7 +1,7 @@
 package com.servegame.bl4de.tag;
 
 import com.servegame.bl4de.common.TagPlugin;
-import org.spongepowered.api.text.Text;
+import com.servegame.bl4de.tag.command.Command;
 
 import java.util.*;
 
@@ -13,19 +13,18 @@ import static com.google.common.base.Preconditions.checkNotNull;
  * @author Brandon Bires-Navel (brandonnavel@outlook.com)
  */
 public class AbstractCommandManager {
-    private Map<TagPlugin, Set<Text>> usages;
+    private Map<TagPlugin, Set<Command>> usages;
 
     public AbstractCommandManager() {
         this.usages = new HashMap<>();
     }
 
-    public void registerCommandUsage(TagPlugin plugin, Text commandUsage){
+    public void registerCommandUsage(TagPlugin plugin, Command command){
         checkNotNull(plugin, "TagPlugin can't be null!");
-        checkNotNull(commandUsage, "Command Usage can't be null!");
-        this.usages.computeIfAbsent(plugin, k -> new HashSet<>()).add(commandUsage);
+        this.usages.computeIfAbsent(plugin, k -> new HashSet<>()).add(command);
     }
 
-    public List<Text> getCommandUsages(TagPlugin plugin){
+    public List<Command> getCommands(TagPlugin plugin){
         return new ArrayList<>(this.usages.get(plugin));
     }
 }
