3a0b20ec7c61e0423229836f43c14f9b7f8910b4
TheCahyag <brandonnavel@outlook.com>
2018-07-29 19:09:41 -0400

    Created a class for representing the end results of a game

---
+++ tag-common/src/main/java/com/servegame/bl4de/common/model/PostGameStats.java
@@ -0,0 +1,21 @@
+package com.servegame.bl4de.common.model;
+
+import com.servegame.bl4de.common.command.element.Duration;
+
+import java.util.List;
+
+/**
+ * File: PostGameStats.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public class PostGameStats {
+
+    private String name;
+
+    private Duration duration;
+
+    private List<AbstractPlayer> activePlayers;
+
+
+}
