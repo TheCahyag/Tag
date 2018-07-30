86a6355cd77a08dd3dac032d5fec02ccfe44a429
TheCahyag <brandonnavel@outlook.com>
2018-07-29 20:17:31 -0400

    Changed the state that keeps track of all the mini games to a ConcurrentHashMap and implemented a method to determine if a player is already involved in a mini game

--- tag-common/src/main/java/com/servegame/bl4de/common/AbstractGameManager.java
+++ tag-common/src/main/java/com/servegame/bl4de/common/AbstractGameManager.java
@@ -4,7 +4,9 @@ import com.servegame.bl4de.common.model.AbstractPlayer;
 import com.servegame.bl4de.common.model.AbstractWorld;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * File: AbstractGameManager.java
@@ -13,23 +15,39 @@ import java.util.List;
  */
 public abstract class AbstractGameManager {
 
-    protected List<AbstractMiniGame> runningGames;
+    protected ConcurrentHashMap<String, AbstractMiniGame> runningGames;
     protected TagPlugin plugin;
 
     public AbstractGameManager(TagPlugin plugin) {
         this.plugin = plugin;
-        this.runningGames = new ArrayList<>();
+        this.runningGames = new ConcurrentHashMap<>();
     }
 
     public List<AbstractMiniGame> getRunningGames() {
-        return runningGames;
+        return new ArrayList<>(runningGames.values());
     }
 
-    public abstract AbstractMiniGame createMiniGame(List<AbstractPlayer> players, AbstractWorld world);
+    public abstract AbstractMiniGame createMiniGame(String name, AbstractWorld world);
 
     public void startMiniGame(AbstractMiniGame miniGame){
         if (this.runningGames.contains(miniGame)){
             miniGame.start();
         }
+    }
+
+    /**
+     * Determines if the given {@link AbstractPlayer} is currently involved in a game as a non-spectator.
+     * @param player given {@link AbstractPlayer}
+     * @return true if the player is already in a game, false otherwise
+     */
+    protected boolean isPlayerCurrentlyInGame(AbstractPlayer player) {
+        Collection<AbstractMiniGame> miniGames = this.runningGames.values();
+        for (AbstractMiniGame miniGame :
+                miniGames) {
+            if (miniGame.hasPlayer(player)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
